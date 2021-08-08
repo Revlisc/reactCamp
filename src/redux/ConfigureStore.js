e960568ad2bc9453ee16e8e840308576d9a25960
Dale <dalesimmons0210@gmail.com>
2021-08-08 12:21:31 -0700

    react redux forms revisited

--- src/redux/ConfigureStore.js
+++ src/redux/ConfigureStore.js
@@ -3,6 +3,8 @@ import { Campsites } from './Campsites';
 import { Comments } from './Comments';
 import { Partners } from './Partners';
 import { Promotions } from './Promotions';
+import { createForms } from 'react-redux-form';
+import { InitialFeedback } from './forms';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 
@@ -12,7 +14,10 @@ export const ConfigureStore = () => {
             campsites: Campsites,
             comments: Comments,
             partners: Partners,
-            promotions: Promotions
+            promotions: Promotions,
+            ...createForms({
+                feedbackForm: InitialFeedback
+            })
         }),
         applyMiddleware(thunk, logger)
     );
