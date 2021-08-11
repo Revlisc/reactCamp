0da1b9596062dfb631c65b486c625be75550024b
dales <dalesimmons0210@gmail.com>
2021-08-11 08:38:39 -0700

    Fetch from Server

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import { Breadcrumb, BreadcrumbItem, Button, Form, FormGroup, Label, Input, Col, FormFeedback, Row } from 'reactstrap';
+import { Breadcrumb, BreadcrumbItem, Button, FormGroup, Label, Input, Col, FormFeedback, Row } from 'reactstrap';
 import { Link } from 'react-router-dom';
-import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Control, Form, Errors } from 'react-redux-form';
 
 const required = val => val && val.length;
